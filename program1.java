b1fff76d33623aad883802823c9b11fd0a3d5cb9
Suchi Goel <suchig946@gmail.com>
2026-06-11 11:43:13 +0530

    Update program1.java

--- program1.java
+++ program1.java
@@ -1,9 +1,17 @@
-class program1 {
-    public class program {
-        int num1;
-        int num2;
-        int scanner sc = sc.nextln;
-        
+import java.util.Scanner;
 
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
     }
 }
